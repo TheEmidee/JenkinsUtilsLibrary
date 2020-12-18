2e2dd77acea570c13032010fc1a0627bfa975197
michael.delva <michael@emidee.net>
2020-12-18 17:40:12 +0100

    update to environment

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -2,9 +2,9 @@ package org.emidee.jenkins;
 
 import groovy.transform.Field
 
-class environment {
-    BranchType BRANCH_TYPE = BranchType.Development
-    BuildConfiguration BUILD_CONFIGURATION = BuildConfiguration.Development
-    DeploymentEnvironment DEPLOYMENT_ENVIRONMENT = DeploymentEnvironment.Development
-    String PROJECT_NAME = ""
+class environment implements Serialization {
+    public BranchType BRANCH_TYPE = BranchType.Development
+    public BuildConfiguration BUILD_CONFIGURATION = BuildConfiguration.Development
+    public DeploymentEnvironment DEPLOYMENT_ENVIRONMENT = DeploymentEnvironment.Development
+    public String PROJECT_NAME = ""
 }
