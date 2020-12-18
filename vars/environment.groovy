0f52e0e4b253d174e339e67b6eb143949431ee0c
michael.delva <michael@emidee.net>
2020-12-18 17:42:39 +0100

    Serializable, not Serialization

--- vars/environment.groovy
+++ vars/environment.groovy
@@ -2,7 +2,7 @@ package org.emidee.jenkins;
 
 import groovy.transform.Field
 
-class environment implements Serialization {
+class environment implements Serializable {
     public BranchType BRANCH_TYPE = BranchType.Development
     public BuildConfiguration BUILD_CONFIGURATION = BuildConfiguration.Development
     public DeploymentEnvironment DEPLOYMENT_ENVIRONMENT = DeploymentEnvironment.Development
