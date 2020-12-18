15d42b298a8ae26e29aff9efb2b797f2ebd0bb4b
michael.delva <michael@emidee.net>
2020-12-18 17:14:52 +0100

    Added default values

--- src/org/emidee/jenkins/Environment.groovy
+++ src/org/emidee/jenkins/Environment.groovy
@@ -2,7 +2,7 @@ package org.emidee.jenkins;
 
 import groovy.transform.Field
 
-@Field BranchType BRANCH_TYPE
-@Field BuildConfiguration BUILD_CONFIGURATION
-@Field DeploymentEnvironment DEPLOYMENT_ENVIRONMENT
-@Field String PROJECT_NAME
+@Field BranchType BRANCH_TYPE = BranchType.Development
+@Field BuildConfiguration BUILD_CONFIGURATION = BuildConfiguration.Development
+@Field DeploymentEnvironment DEPLOYMENT_ENVIRONMENT = DeploymentEnvironment.Development
+@Field String PROJECT_NAME = ""
