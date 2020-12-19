9f8d25d3b8056c4692dfdfa9172c25e7cd2a18a8
Michael Delva <michael@emidee.net>
2020-12-19 17:21:47 +0100

    Missing import

--- vars/getGitHubPullRequestInfos.groovy
+++ vars/getGitHubPullRequestInfos.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/groovy
 
+import groovy.json.JsonSlurper
+
 def call( String github_token, String repository_owner, String repository_name, String pull_request_id ) {
     String url = "https://api.github.com/repos/${repository_owner}/${repository_name}/pulls/${pull_request_id}"
     
