83c4351d9cd19c56202913bdebcdc467e18caa39
Michael Delva <michael@emidee.net>
2020-12-19 21:24:32 +0100

    Return string

--- vars/getGitHubPullRequestInfos.groovy
+++ vars/getGitHubPullRequestInfos.groovy
@@ -1,12 +1,13 @@
 #!/usr/bin/groovy
 
-import groovy.json.JsonSlurper
+//import groovy.json.JsonSlurper
 
 def call( String github_token, String repository_owner, String repository_name, String pull_request_id ) {
     String url = "https://api.github.com/repos/${repository_owner}/${repository_name}/pulls/${pull_request_id}"
     
     def text = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
-    def json = new JsonSlurper().parseText( text )
+    return text
 
-    return json
+    // def json = new JsonSlurper().parseText( text )
+    // return json
 }
