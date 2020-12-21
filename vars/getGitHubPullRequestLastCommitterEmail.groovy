8d728994847103ec9b5154af8a412aca41c3ccb8
Michael Delva <michael@emidee.net>
2020-12-21 17:32:03 +0100

    Pass script to all gh helper functions

--- vars/getGitHubPullRequestLastCommitterEmail.groovy
+++ vars/getGitHubPullRequestLastCommitterEmail.groovy
@@ -2,9 +2,9 @@
 
 import groovy.json.JsonSlurper
 
-def call( String github_token, String repository_owner, String repository_name, String pull_request_id ) {
+def call( Script script, String github_token, String repository_owner, String repository_name, String pull_request_id ) {
     def json_string = getGitHubPullRequestCommits( github_token, repository_owner, repository_name, pull_request_id )
-    def commits = new JsonSlurper().parseText( json_string )
+    def commits = script.readJSON text: json_string
 
     def commits_count = commits.size()
     def last_commit = commits[ commits_count - 1 ]
