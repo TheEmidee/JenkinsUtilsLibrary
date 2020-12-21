a2246c11b7d61491da7669e39f23ac5f7bf14e88
Michael Delva <michael@emidee.net>
2020-12-21 17:19:02 +0100

    Made getGitHubPullRequestInfos return a JSON

--- vars/getGitHubPullRequestInfos.groovy
+++ vars/getGitHubPullRequestInfos.groovy
@@ -1,8 +1,12 @@
 #!/usr/bin/groovy
 
+import groovy.json.JsonSlurper
+
 def call( String github_token, String repository_owner, String repository_name, String pull_request_id ) {
     String url = "https://api.github.com/repos/${repository_owner}/${repository_name}/pulls/${pull_request_id}"
     
-    def text = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
-    return text
+    def json_string = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
+    def pull_request_infos = new JsonSlurper().parseText( json_string )
+
+    return pull_request_infos
 }
