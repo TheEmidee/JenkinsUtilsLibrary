9794cec1b2412aac0d14b48e2cae2ec97d54dd8d
Michael Delva <michael@emidee.net>
2020-12-21 17:27:40 +0100

    Pass script to getGitHubPullRequestInfos to call readJSON

--- vars/getGitHubPullRequestInfos.groovy
+++ vars/getGitHubPullRequestInfos.groovy
@@ -1,13 +1,11 @@
 #!/usr/bin/groovy
 
-import groovy.json.JsonSlurper
-
-@NonCPS
-def call( String github_token, String repository_owner, String repository_name, String pull_request_id ) {
+def call( Script script, String github_token, String repository_owner, String repository_name, String pull_request_id ) {
     String url = "https://api.github.com/repos/${repository_owner}/${repository_name}/pulls/${pull_request_id}"
     
     def json_string = url.toURL().getText( requestProperties: [ 'Authorization' : "token ${github_token}" ] )
-    def pull_request_infos = new JsonSlurper().parseText( json_string )
+    
+    def pull_request_infos = script.readJSON text: json_string
 
     return pull_request_infos
 }
