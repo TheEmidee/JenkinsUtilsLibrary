398a25e3ef8c6a1737f3d71c38ca19d80903f9fa
michael.delva <michael@emidee.net>
2020-12-21 14:04:02 +0100

    Added getGitHubPullRequestLastCommitterEmail

---
+++ vars/getGitHubPullRequestLastCommitterEmail.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+
+import groovy.json.JsonSlurper
+
+def call( String github_token, String repository_owner, String repository_name, String pull_request_id ) {
+    def json_string = getGitHubPullRequestCommits( github_token, repository_owner, repository_name, pull_request_id )
+    def commits = new JsonSlurper().parseText( json_string )
+
+    def commits_count = commits.size()
+    def last_commit = commits[ commits_count - 1 ]
+    log.info "Last commit SHA : ${last_commit.sha}"
+
+    pr_last_committer_email = last_commit.commit.author.email
+    log.info "Last commit author email : ${pr_last_committer_email}"
+
+    return text
+}
