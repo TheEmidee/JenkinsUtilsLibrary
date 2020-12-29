8521d5755049ddf1abb5574a7a420bf1b4f94fbb
michael.delva <michael@emidee.net>
2020-12-29 16:11:16 +0100

    Return the success of the robocopy operation

--- vars/roboCopy.groovy
+++ vars/roboCopy.groovy
@@ -2,8 +2,11 @@
 
 def call( source, destination, arguments ) {
     def status = bat returnStatus: true, script: "robocopy.exe ${source} ${destination} ${arguments}"
+    def success = !(status < 0 || status > 3)
 
-    if (status < 0 || status > 3) {
+    if ( !success ) {
         log.fatal( "ROBOCOPY failed" )
     }
+
+    return success
 }
