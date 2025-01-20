98730f285dfe6482aa728c7084e8c4778b28ce1b
michael.delva <michael.delva@fishingcactus.com>
2025-01-20 15:29:13 +0100

    getWorkspace can accept a suffix

--- vars/getWorkspace.groovy
+++ vars/getWorkspace.groovy
@@ -5,7 +5,7 @@ import org.emidee.jenkins.BuildConfiguration
 import org.emidee.jenkins.DeploymentEnvironment
 import org.emidee.jenkins.Environment
 
-def call( Script script ) {
+def call( Script script, String suffix = "" ) {
     
     def node_workspace = pwd()
     def parent_workspace = new File( node_workspace ).parent
@@ -13,6 +13,11 @@ def call( Script script ) {
     project_workspace_name += ( Environment.instance.DEPLOYMENT_ENVIRONMENT as DeploymentEnvironment ) == DeploymentEnvironment.Shipping
         ? "_Master"
         : "_Develop"
+
+
+    if (suffix?.trim()) {
+        project_workspace_name += "_" + suffix
+    }
 
     def workspace = new File( parent_workspace, project_workspace_name ).toString()
     
